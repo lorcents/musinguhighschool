cbbdd3d69d7d27ef86f79fa2f88d48213acaf29d
Ian Njoroge <53985098+lorcents@users.noreply.github.com>
2020-04-12 07:47:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>musinguhigh</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="images/f.JPG" type="">
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="subpage">
+
+			
+
+		<header id="header">
+			<div class="inner">
+				<a href="index.html" class="logo">Home</a>
+				<nav id="nav">
+						<a href="index.html">HOME</a>
+						<a href="about.html">ABOUT US</a>
+						<a href="contacts.html">CONTACT</a>
+						
+						<a href="students.html">STUDENTS</a>
+						<a href="teachers.html">WALIMU</a>
+						<a href="Alumini/Alumini.html">ALUMINI</a>
+						
+				</nav>
+				<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+			</div>
+		</header>
+		<section id="banner">
+			<h1>Welcome to musinguhigh</h1>
+			<p>The school of choice</p>
+		</section>
+
+
+			<section id="one" class="wrapper">
+				<div class="inner">
+					<div class="flex flex-3">
+						<article>
+							<header>
+								<h3>Brief history<br />of musinguhigh</h3>
+							</header>
+							<p><strong>Musingu  Boys was started in the early sixtees with a bunch of Friends church missionaries..
+								The first Prncipal of the school was Mr Laban Ayiro.The school was based in christian moral and values.
+								The school is located in Western region Kakamega county,shinyalu sub county around Malinya market
+								.</strong></p>
+							
+								
+							
+						</article>
+						<article>
+							<header>
+								<h3>The Native origin<br /> </h3>
+							</header>
+							<p><strong> The name musingu was derived from an ancient tree known as musingu tree in luhya land 
+								The tree is almost to extinction ,so the one tree in the schoool compound next to the admistration
+								block is preserved and mantained by all means.As to expert The original musingu tree is to extinction
+								and the current refeered tree next to adminstration building is a regrown outcome of another species
+								on the original musingu trree.
+								</strong>
+							</p>
+							
+						</article>
+						<article>
+							<header>
+								<h3>The school motto<br /> vision and mission</h3>
+							</header>
+							<p><strong>The  school motto states NIDHAMU NA BIDII<br>
+								OUR VISION: To empact modernn teaching and practices to prepare young boys into great men<br>
+								OUR MISSION: Be part of the mode changing face of the country gearing towards a smart country technologicaly
+								and idealy by 2030.
+							</strong></p>
+							
+						</article>
+					</div>
+				</div>
+			</section>
+
+		
+			<section id="two" class="wrapper style1 special">
+				<div class="inner">
+					<header>
+						<h2>THE SCHOOL OUTLOOK</h2>
+						
+					</header>
+					<div class="flex flex-4">
+						<div class="box person">
+							<div class="image fixed">
+								<img src="images/b.JPG" alt="admin" />
+							</div>
+							<h3>The school admistration block</h3>
+							<p>It was opened in 2008.The construction of the block was fully funded by school funds</p>
+						</div>
+						<div class="box person">
+							<div class="image oval">
+								<img src="images/a.JPG" alt="" />
+							</div>
+							<h3>The school Gate</h3>
+							<p>Musingu High school gate.The school  gate was a gift from an old boy who felt to give back
+								by renovating the school gate.Was a gift from Mr Hillary Ayoi class of 1998.
+							</p>
+						</div>
+						<div class="box person">
+							<div class="image fixed">
+								<img src="images/d.JPG" alt=" " />
+							</div>
+							<h3>The School principa</h3>
+							<p>Dr Aliva Luvaso the school principal with His Deputy.
+							</p>
+						</div>
+						<div class="box person">
+							<div class="image fixed">
+								<img src="images/e.JPG" alt="Principal" />
+							</div>
+							<h3>The school Bus</h3>
+							<p>Populaly known as New Down.The Bus was purchased early 2014
+								fully sponsored by the parents association.The school owns one more scania bus
+								and a school Van.
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="three" class="wrapper special">
+				<div class="inner">
+					<header class="align-center">
+						<h2>The School of choice</h2>
+						<p> </p>
+					</header>
+					<div class="flex flex-2">
+						<article>
+							<div class="image fit">
+								<video src="images/me.mp4"width="640"height="400"controls autoplay="off"></video>
+
+							</div>
+							<header>
+								<h3>President Uhuru stops by</h3>
+							</header>
+							<p><strong>  After a mean of 10.3 last year President Uhuru Kenyatta made a suprise visit to congradulate the students
+								The school was the best in Kakamega County and third Best in The western region.
+							   With a rich history of academic excellance Musingu Boys High school has been among 
+							   the best institution in the country.Although the Ministry of education upheld ranking of schools
+							   a local survey indicates that our school was twenty third based in the entire country
+							   </strong>
+							</p>
+							<strong><uL>yearly mean score
+								<li>2018-10.3 B+</li><hr>
+								<li>2017-9.2   B</li><hr>
+								<li>2016-9.3  B</li><hr>
+								<li>2015-10.3  B+</li><hr>
+								<li>2014-9.0   B</li><hr>
+								<li>2013-8.3  B-</li><hr>
+							</uL>
+							</strong>
+
+						</article>
+						<article>
+							<div class="image fit">
+									<img src="images/f.JPG"alt="">
+							</div>
+							<header>
+								<h3>Our school badge</h3><br><p><strong>NIDHAMU NA BIDII</strong></p>
+							</header>
+							<h3>school anthem</h3>
+							
+				<iframe src="anthem.html"width="400"height="250">
+        <a href="anthem.html">anthem</a>
+				</iframe>
+							
+						</article>
+					</div>
+				</div>
+			</section>
+
+		
+			<footer id="footer">
+				<div class="inner">
+					<div class="flex">
+						<div class="copyright">
+							<p><strong>copyright &copy musinguhigh2019</strong></p>
+						</div>
+						<ul class="icons">
+							<p><strong> Follow us on</strong></p>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instragram</span></a></li>
+					
+					
+							
+						</ul>
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
